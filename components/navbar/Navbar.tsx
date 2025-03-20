03e912ecc3eb207266d42be56da5299eb64fc619
David McGee <jdavidmcgee@mac.com>
2025-03-20 09:52:33 -0500

    13th commit.  add the Suspense wrapper in the Navbar.tsx component - around the NavSearch component.

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from 'react';
 import NavSearch from './NavSearch';
 import LinksDropdown from './LinksDropdown';
 import DarkMode from './DarkMode';
@@ -6,9 +7,11 @@ import Logo from './Logo';
 export default function Navbar() {
 	return (
 		<nav className="border-b">
-			<div className="container flex flex-col sm:flex-row sm: justify-between sm:items-center flex-wrap gap-4 py-8">
+			<div className="container flex flex-col sm:flex-row sm:justify-between sm:items-center flex-wrap gap-4 py-8">
 				<Logo />
-				<NavSearch />
+				<Suspense fallback={<div>Loading search...</div>}>
+					<NavSearch />
+				</Suspense>
 				<div className="flex gap-4 items-center">
 					<DarkMode />
 					<LinksDropdown />
